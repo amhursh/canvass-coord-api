fce29ace656d3252302d6c3a363882c26deba009
Aaron Hursh <amhursh@gmail.com>
2017-12-21 01:30:06 -0700

    Add test for root path

--- server/app.js
+++ server/app.js
@@ -6,8 +6,9 @@ const PORT = process.env.PORT || 8081
 
 app.use(rootRoute.routes())
 
-const server = app.listen(PORT).on('error', err => {
-  console.error(err)
-})
+if(!module.parent)
+  app.listen(PORT).on('error', err => {
+    console.error(err)
+  })
 
-module.exports = server
+module.exports = app
--- server/controllers/rootController.js
+++ server/controllers/rootController.js
@@ -2,12 +2,12 @@ const env = process.env.NODE_ENV || 'development'
 const config = require('../../knexfile')[env]
 const db = require('knex')(config)
 
-const endpoints = {
-  root: '/'
-}
+// const endpoints = {
+//   root: '/'
+// }
 
 const index = async ctx => {
-  ctx.body = endpoints
+  ctx.body = 'CanvassCoord API Service'
 }
 
 module.exports = { index }
