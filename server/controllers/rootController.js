c12c23bf62c3827759e05aaa481d9c406fa84bcc
Aaron Hursh <amhursh@gmail.com>
2017-12-21 22:33:01 -0700

    Add targets model and all function

--- server/controllers/rootController.js
+++ server/controllers/rootController.js
@@ -2,10 +2,6 @@ const env = process.env.NODE_ENV || 'development'
 const config = require('../../knexfile')[env]
 const db = require('knex')(config)
 
-// const endpoints = {
-//   root: '/'
-// }
-
 const index = async ctx => {
   ctx.body = 'CanvassCoord API Service'
 }
