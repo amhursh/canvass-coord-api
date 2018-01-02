48a44653a2c825fd3781ef44e290235d600d1a4a
Aaron Hursh <amhursh@gmail.com>
2018-01-02 02:13:06 -0700

    Add prelim create action

--- server/controllers/campaignsController.js
+++ server/controllers/campaignsController.js
@@ -2,6 +2,8 @@ const env = process.env.NODE_ENV || 'development'
 const config = require('../../knexfile')[env]
 const db = require('knex')(config)
 
+pry = require('pryjs')
+
 Campaigns = require('../models/campaign')
 
 const index = async ctx => {
@@ -13,6 +15,16 @@ const index = async ctx => {
   ctx.body = campaigns
 }
 
+const create = async ctx => {
+  let title = ctx.request.body.campaign.title
+  let description = ctx.request.body.campaign.description
+  // eval(pry.it)
+  let campaigns = await Campaigns.addCamp(title, description)
+
+  eval(pry.it)
+}
+
 module.exports = {
-  index
+  index,
+  create
 }
