a42c403c2a830ba20061d57ee1ff6dc2ed9e6b1e
Aaron Hursh <amhursh@gmail.com>
2018-01-07 16:23:11 -0700

    Add survey routes to app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('koa-bodyparser')
 const rootRoute = require('./routes/root.routes')
 const targetsRoutes = require('./routes/targets.routes')
 const campaignsRoutes = require('./routes/campaigns.routes')
+const surveysRoutes = require('./routes/surveys.routes')
 
 const app = new Koa()
 const PORT = process.env.PORT || 8080
@@ -13,6 +14,7 @@ app.use(bodyParser())
 app.use(rootRoute.routes())
 app.use(targetsRoutes.routes())
 app.use(campaignsRoutes.routes())
+app.use(surveysRoutes.routes())
 
 if(!module.parent)
   app.listen(PORT).on('error', err => {
