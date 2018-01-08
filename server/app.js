7c4dd1c4e13148fb6c21758553feea8883475147
Aaron Hursh <amhursh@gmail.com>
2018-01-08 04:22:43 -0700

    Add answers to server

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const targetsRoutes = require('./routes/targets.routes')
 const campaignsRoutes = require('./routes/campaigns.routes')
 const surveysRoutes = require('./routes/surveys.routes')
 const campaignTargetsRoutes = require('./routes/campaigns/targets.routes')
+const answersRoutes = require('./routes/answers.routes')
 
 const app = new Koa()
 const PORT = process.env.PORT || 8080
@@ -17,6 +18,7 @@ app.use(targetsRoutes.routes())
 app.use(campaignsRoutes.routes())
 app.use(surveysRoutes.routes())
 app.use(campaignTargetsRoutes.routes())
+app.use(answersRoutes.routes())
 
 if(!module.parent)
   app.listen(PORT).on('error', err => {
--- server/models/survey.js
+++ server/models/survey.js
@@ -3,7 +3,7 @@ const config = require('../../knexfile')[env]
 const db = require('knex')(config)
 
 const byCampaign = (campaignId) => {
-  return db.select('id', 'title').table('surveys').where('campaign_id', campaignId)
+  return db.select().table('surveys').where('campaign_id', campaignId)
 } 
 
 module.exports = {
