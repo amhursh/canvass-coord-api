80448b8cec85b8ece6190ed0de081fe6db68e32d
Aaron Hursh <amhursh@gmail.com>
2018-01-08 04:55:14 -0700

    Add answer model and add answer query

---
+++ server/models/answer.js
@@ -0,0 +1,9 @@
+const env = process.env.NODE_ENV || 'development'
+const config = require('../../knexfile')[env]
+const db = require('knex')(config)
+
+const addAnswer = (answer, questionId, targetId) => {
+  return db('answers')
+    .insert({body: answer, question_id: questionId, target_id: targetId})
+    .returning('*')
+}
